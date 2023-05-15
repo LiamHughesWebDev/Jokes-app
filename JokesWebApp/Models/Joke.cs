fcd0b795eca451b9738deb8d2bd150afe9cf719c
Liam Hughes <ljhughes599@gmail.com>
2023-05-15 15:08:12 -0400

    Publish to Github

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
